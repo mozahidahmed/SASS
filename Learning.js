11bec852f96b208babb4e5339ca0ad77680458d0
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-03-27 23:10:00 +0600

    @forword

--- Learning.js
+++ Learning.js
@@ -2,6 +2,10 @@
 01. Install live sass compiler
 02. Click watch sass
 03. Create a variable 
+04 . import and partial => @forword demo.scss file
+     or you can import all file in index.sass file and you just 
+     import index.sass file in demo.sasss fiule
+05.
 
 
 
