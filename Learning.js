9781223051c572a0cc3c08d776abe62bc5eb9bd1
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-03-28 16:41:44 +0600

    mixin and parameter

--- Learning.js
+++ Learning.js
@@ -5,7 +5,25 @@
 04 . import and partial => @forword demo.scss file
      or you can import all file in index.sass file and you just 
      import index.sass file in demo.sasss fiule
-05.
+05. @mixin   @include
+     @mixin para-style{
+     padding: 50px;
+     background-color: aqua;
+     }
+
+     p{
+     @include para-style()
+     }
+06,parameter
+     @mixin para-style($size,$bg){
+     padding: $size;
+     background-color: $bg;
+     }
+
+     p{
+     @include para-style(12,yellow);
+     }
+07,
 
 
 
